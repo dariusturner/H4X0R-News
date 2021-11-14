73a0e89d1a36bf4e6291f3f7d1c261196630642c
Darius Turner <dariusot731@gmail.com>
2021-11-14 08:43:01 -0500

    Networking Setup Complete
    Added functionality to grab data from the HN Search API to show data in the app.

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4X0R News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -24,13 +33,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bye"),
-    Post(id: "3", title: "Goodnight")
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bye"),
+//    Post(id: "3", title: "Goodnight")
+//]
